8ebe3f043f8b4e3daa07d5eeaf0884777c8ef15b
mohammedkaazim <umohammedkaazim@gmail.com>
2025-09-16 22:31:35 +0530

    Main

---
+++ smart-blood-bank/src/main/java/com/bloodbank/SmartBloodBankApplication.java
@@ -0,0 +1,13 @@
+package com.bloodbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBloodBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBloodBankApplication.class, args);
+		System.out.println("Smart Blood Bank System Started Successfully...");
+	}
+}
